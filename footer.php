d102de754713f65452158876d959d7ddb20fb934
Tiago Mergulhão <me@tmergulhao.com>
2015-07-14 11:37:28 -0300

    Bootstrap styling of footer

--- footer.php
+++ footer.php
@@ -1,30 +1,25 @@
 		</div><!-- /main -->
 
-
-
-		<footer id="main-footer" class="span-24 site-footer">
-			<div class="container">
-
-				<div class="social">
-					<ul class="social-links">
-						<li class="twitter"><a href="https://twitter.com/culturagovbr" target="_blank" title="Twitter Cultura"><i class="fa fa-twitter"></i></a></li>
-						<li class="facebook"><a href="https://www.facebook.com/MinisterioDaCultura" target="_blank" title="Facebook Cultura"><i class="fa fa-facebook"></i></a></li>
-						<li class="instagram"><a href="https://instagram.com/culturagovbr/" target="_blank" title="Instagram Cultura"><i class="fa fa-instagram"></i></a></li>
-						<li class="youtube"><a href="https://www.youtube.com/user/ministeriodacultura" target="_blank" title="Youtube Cultura"><i class="fa fa-youtube-square"></i></i></a></li>
-						<li class="tumblr"><a href="http://culturagovbr.tumblr.com/" target="_blank" title="Tumblr Cultura"><i class="fa fa-tumblr"></i></a></li>
-						<li class="flickr"><a href="https://www.flickr.com/photos/ministeriodacultura" target="_blank" title="Flickr Cultura"><i class="fa fa-flickr"></i></a></li>
-					</ul>
+		<footer id="site-footer" class="site-footer row">
+				<div class="social col-xs-8 col-xs-offset-2">
+					<a class="col-xs-2 text-center fa fa-twitter twitter" href="https://twitter.com/culturagovbr" target="_blank" title="Twitter Cultura"></a>
+					<a class="col-xs-2 text-center fa fa-facebook facebook" href="https://www.facebook.com/MinisterioDaCultura" target="_blank" title="Facebook Cultura"></a>
+					<a class="col-xs-2 text-center fa fa-instagram instagram" href="https://instagram.com/culturagovbr/" target="_blank" title="Instagram Cultura"></a>
+					<a class="col-xs-2 text-center fa fa-youtube-square youtube" href="https://www.youtube.com/user/ministeriodacultura" target="_blank" title="Youtube Cultura"></i></a>
+					<a class="col-xs-2 text-center fa fa-tumblr tumblr" href="http://culturagovbr.tumblr.com/" target="_blank" title="Tumblr Cultura"></a>
+					<a class="col-xs-2 text-center fa fa-flickr flickr" href="https://www.flickr.com/photos/ministeriodacultura" target="_blank" title="Flickr Cultura"></a>
 				</div>
 
-				<div class="logos">
-					<div class="minc"><a href="http://www.cultura.gov.br" title="Ministério da Cultura - Governo Federal"><img src='<?php echo get_template_directory_uri(); ?>/images/minc-gov.png'/></a></div>
+				<div class="logos col-xs-12">
+					<a id="PNC_logo" class="col-xs-4" href="http://www.cultura.gov.br" title="Ministério da Cultura - Governo Federal"><img class="img-responsive" src='<?php echo get_template_directory_uri(); ?>/images/pnc_logo.png'/></a>
+					<a id="CNPC_logo" class="col-xs-4" href="http://www.cultura.gov.br" title="Ministério da Cultura - Governo Federal"><img class="img-responsive" src='<?php echo get_template_directory_uri(); ?>/images/CNPC_logo.png'/></a>
+					<a id="MinC_logo" class="col-xs-4" href="http://www.cultura.gov.br" title="Ministério da Cultura - Governo Federal"><img class="img-responsive" src='<?php echo get_template_directory_uri(); ?>/images/MINC_logo.png'/></a>
 				</div>
 
-				<p class="creditos">
-					<span class="textleft">2015 &#169; Conselho Nacional de Política Cultural. Alguns direitos reservados.</br>
-					Endereço: Esplanada dos Ministérios, Bloco B, 3º andar, CEP 70068-900, Brasília - Distrito Federal, Telefone: (61) 2024-2361/2302</a></span>
-				</p>
-			</div>
+				<div class="creditos col-xs-6 col-xs-offset-3">
+					<p class="text-center">2015 &#169; Conselho Nacional de Política Cultural. Alguns direitos reservados.</p>
+					<p class="text-center">Endereço: Esplanada dos Ministérios, Bloco B, 3º andar, CEP 70068-900, Brasília - Distrito Federal, Telefone: (61) 2024-2361/2302</p>
+				</div>
 		</footer> 
 	</div><!-- /site-wrap -->
 
--- includes/jquery.php
+++ includes/jquery.php
@@ -1,4 +1,4 @@
-<!-- jQuery-->
+<!-- jQuery -->
 
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
