26e49f70f813f06521c13aac2e55e45e4cecee7e
Tiago Mergulhão <me@tmergulhao.com>
2015-07-15 07:52:13 -0300

    Deprecation of asides class ahead of bootstraping

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 
 if ( is_active_sidebar( 'primary-widget-area' ) ) :
 
-	?><div class="asides widget-area"><?php
+	?><div class="widget-area"><?php
 	
 	if ( is_active_sidebar( 'primary-widget-area' ) ) dynamic_sidebar( 'primary-widget-area' );
 	
