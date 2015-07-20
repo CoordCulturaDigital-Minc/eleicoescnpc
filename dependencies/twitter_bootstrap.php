5de9f9f55eed7ca948aefab4055f3c4edeef109f
Cleber Santos <oclebersantos@gmail.com>
2015-07-20 12:23:14 -0300

    Retirando js não utilizados

--- dependencies/twitter_bootstrap.php
+++ dependencies/twitter_bootstrap.php
@@ -1,5 +1,5 @@
 <!-- Bootstrap requires jQuery -->
-<?php require_once 'jquery.php';?>
+<?php // require_once 'jquery.php';?>
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
@@ -8,8 +8,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
 
 <!-- Mobile first non-scalable, no-pinch -->
 <!-- http://getbootstrap.com -->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
--- header.php
+++ header.php
@@ -13,9 +13,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php wp_title( '&mdash;', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	
-	<?php require_once 'dependencies/jquery.php';?>
+	<?php// require_once 'dependencies/jquery.php';?>
 	<?php require_once 'dependencies/twitter_bootstrap.php'; ?>
 	<?php require_once 'dependencies/font_awesome.php'; ?>
 
