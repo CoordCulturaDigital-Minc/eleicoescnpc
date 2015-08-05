4c10230f41a79e6ef3d208e834840d9cc37414be
Tiago Mergulhão <me@tmergulhao.com>
2015-08-05 15:42:07 -0300

    External border

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 
 if ( is_active_sidebar( 'primary-widget-area' ) ) :
 
-	?><div class="col-xs-12 col-md-4"><?php
+	?><div class="sidebar col-xs-12 col-md-4"><?php
 	
 	if ( is_active_sidebar( 'primary-widget-area' ) ) dynamic_sidebar( 'primary-widget-area' );
 	
