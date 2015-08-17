b03115dd0d60b7a5b5023534b061022e169ded25
Tiago Mergulhão <me@tmergulhao.com>
2015-08-17 14:08:12 -0300

    Favicon correction

--- includes/favicons.php
+++ includes/favicons.php
@@ -1,28 +1,28 @@
 <!-- Courtesy of audreyr -->
 <!-- https://github.com/audreyr/favicon-cheat-sheet -->
 
-<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>favicon.ico">
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() . '/favicons/' ?>favicon.ico">
 
-<link rel="icon" sizes="any" mask="" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>favicon.svg">
+<link rel="icon" sizes="any" mask="" href="<?php echo get_template_directory_uri() . '/favicons/' ?>favicon.svg">
 <meta name="theme-color" content="#4078c0">
 
 <meta name="msapplication-TileColor" content="#16642B">
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); . '/favicons/' ?>ms-favicon-144.png">
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri() . '/favicons/' ?>ms-favicon-144.png">
 
 <!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
-<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-152.png">
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-152.png">
 
 <!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-144.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-144.png">
 
 <!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-120.png">
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-120.png">
 
 <!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-114.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-114.png">
 
 <!-- For first- and second-generation iPad: -->
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-72.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-72.png">
 
 <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); . '/favicons/' ?>appl-favicon-57.png">
+<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri() . '/favicons/' ?>appl-favicon-57.png">
