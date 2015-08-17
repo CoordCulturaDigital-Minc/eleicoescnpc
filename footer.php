bbe615e499c8b3b14b6642e8ecce09a3f32ae8ba
Ciscnick <jesselukas@gmail.com>
2015-08-17 18:32:29 -0300

    Inclusão do Script PIWIK

--- footer.php
+++ footer.php
@@ -77,6 +77,23 @@ if ( $mostra_query->have_posts() ) : ?>
 		<?php endwhile; ?>
 	</div><!-- /logos -->
 <?php endif; ?>
+
+<!— Piwik —>
+<script type="text/javascript">
+ var _paq = _paq || [];
+ _paq.push(["trackPageView"]);
+ _paq.push(["enableLinkTracking"]);
+
+ (function() {
+ var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analise.cultura.gov.br/";
+ _paq.push(["setTrackerUrl", u+"piwik.php"]);
+ _paq.push(["setSiteId", "17"]);
+ var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+ g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+ })();
+</script>
+<!— End Piwik Code —>
+
 <?php wp_reset_postdata(); ?>
 
 <?php wp_footer(); ?>
