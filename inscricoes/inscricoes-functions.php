8cb13a7bf739ee9a05e0f652d975e22fc812ef2c
Cleber Santos <oclebersantos@gmail.com>
2015-09-10 15:27:22 -0300

    Pegando o ip do usuario cadastrado

--- inscricoes/inscricoes-functions.php
+++ inscricoes/inscricoes-functions.php
@@ -1007,3 +1007,18 @@ function is_a_valid_birth($d) {
     return true;
 }
 
+
+function cnpc_get_the_user_ip() {
+
+    if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+        //check ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+        //to check ip is pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return apply_filters( 'wpb_get_ip', $ip );
+}
